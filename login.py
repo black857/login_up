1db6d2ae369233539f4d1d7d9fd4613f8439babf
black <black@2447687327@qq.com>
2020-04-04 20:53:37 +0800

    debug of login

--- login.py
+++ login.py
@@ -1,21 +1,20 @@
 import easygui as gui   #GUI
 import pickle           #写（读取）用户信息
-import random           #随机二维码
+import random           #随机验证码
 import os               #用来写系统路径分割符和......
 
-errortime = 0           #记录输入密码时错误次数。
-find_score = 0          #记录找回密码时的分数
-error_information = []  #记录用户找回密码时填错的选项
-
-os.chdir('E:\\python\\用户登录')
 #以只读二进制模式打开用户信息文件并读取
-f = open('E:\\python\\用户登录\\User','rb')
+os.chdir('E:\\python\\learnpython\\login_up')
+f = open('User','rb')
 user = pickle.load(f)
+f.close()
 
-print(user)
+errortime = 0                   #记录输入密码时错误次数。
+find_score = 0                  #记录找回密码时的分数
+error_information = []          #记录用户找回密码时填错的选项
+b = False                       #记录注册时账号是否存在
+listcodes = os.listdir(r'E:/python/learnpython/login_up/code')  #记录验证码的列表
 
-# 记录验证码的列表
-listcodes = os.listdir('code')
 
 #登录
 def log_in():
@@ -27,6 +26,11 @@ def log_in():
         if field == None:
             return False
 		
+        #判断账号是否为纯数字
+        if not field[0].isdigit():
+            gui.msgbox('账号只可以包含数字')
+            continue
+        
         #判断账号密码是否为空
         if '' in field:
             gui.msgbox(msg = '请输入账号和密码！')
@@ -35,18 +39,18 @@ def log_in():
         #判断账号是否存在
         if field[0] not in user:
             gui.msgbox('改账号不存在')
-            field = gui.multpasswordbox(msg = '请输入账号密码',title = '\t登录\t',fields = ['*账号','*密码'])
             continue
 		
 		#判断密码是否正确
-        if not(field[1] == user[field[0]][5]):
-            gui.msgbox(msg = '密码输入错误！',title = '\tError\t')
-            errortime += 1
-            continue
-
-        #是否找回密码
-        if errortime >= 3:
-            findpassword()
+        if not(field[1] == user[field[0]] [4]):
+            find_choice = gui.buttonbox(msg = '密码输入错误！\n要找回密码吗？',title = '\tError\t',choices = ['重新尝试','找回密码'])
+            if find_choice == '找回密码':
+                if findpassword():
+                    return True
+                else:
+                    continue
+            else:
+                continue
 
         return True    #莫得问题就跳出循环吧。
 	
@@ -54,6 +58,7 @@ def log_in():
 def findpassword():
     global find_score
     while 1:
+
         find_information = gui.multenterbox(msg = '请输入信息',title = '\t找回密码\t',fields = ['账号','用户名','真实姓名','邮箱'])
 			
 		# 判断是否取消
@@ -109,32 +114,41 @@ def  log_up():
         if field == None:
             return False
         # 确认用户是否存在
-        print(user)
         for each_user in user.values():
-            print(each_user)
-            b = field[0]
-            c = each_user[0]
-            if b == c:
-                gui.msgbox('该账号已存在')
+            if field[0] != each_user[0]:
+                b = True
                 break
-            if each_user[1] == field[1]:
-                gui.msgbox('用户名已存在')
+            if each_user[1] != field[1]:
+                b = True
                 break
-        else:
+        
+        if not b:
+            gui.msgbox('账号(或用户名已存在)')
             continue
-        # 随机验证码
-        code = random.choice(listcodes)
-        # 输入验证码
-        index_code = gui.enterbox(msg = '输入验证码',title = '用户注册',image = 'code%s%s' % (os.sep,code))
-        # 检查验证码
-        if index_code == code[3:]:
-            gui.msgbox(msg = '注册成功',title = '成功！')
-            # 记录信息
-            user[field[0]] = field
-            f = open('User','wb')
-            f.write(user)
-            f.close()
-            return True
+        
+        while 1:
+            # 随机验证码
+            code = random.choice(listcodes)
+            # 输入验证码
+            index_code = gui.enterbox(msg = '输入验证码',title = '注册',image = 'code%s%s' % (os.sep,code))
+            
+            # 检查验证码是否为空
+            if index_code == None:
+                return False
+            
+            # 检查验证码
+            if index_code == code[:4]:
+                
+                # 记录信息
+                user[field[0]] = field
+                f = open('User','wb')
+                f.write(user)
+                f.close()
+                return True
+            
+            else:
+                gui.msgbox(msg = '输入错误...')
+                continue
 
 
 
--- pickle_dump.py
+++ pickle_dump.py
@@ -1,8 +1,13 @@
-#-*- coding:ANSI -*-
 import pickle as p
+import os
+
+os.chdir(r'E:/python/learnpython/login_up')
 a = {
-2148537445:['2148537445','ÜöÍ©','xxx','2447687327@qq.com','1593572486','ÄãÊÇË­','cxk']
+'2148537445':['2148537445','huitong','xxx','2447687327@qq.com','1593572486','nijiaosha','cxk']
 }
-f = open('User','rb')
-b = p.load(f)
-print(b)
+f = open('User','wb')
+p.dump(a,f)
+
+# b = p.load(f)
+# print(b)
+f.close()
